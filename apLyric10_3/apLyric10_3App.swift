39fd3ccc191923ba861bead12730ea3df7706bc0
Skidudeaa <Namosson@gmail.com>
2023-10-03 15:31:42 -0500

    Initial Commit

---
+++ apLyric10_3/apLyric10_3App.swift
@@ -0,0 +1,17 @@
+//
+//  apLyric10_3App.swift
+//  apLyric10_3
+//
+//  Created by Thomas Amosson on 2023.10.03.
+//
+
+import SwiftUI
+
+@main
+struct apLyric10_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
